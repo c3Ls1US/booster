d74296268faf1b6033d3a0a59e05416c4a9f08a1
celsius <50845859+c3Ls1US@users.noreply.github.com>
2023-09-23 04:49:29 -0700

    Add comments

--- init/tpm.go
+++ init/tpm.go
@@ -102,11 +102,14 @@ func tpm2Unseal(public, private []byte, pcrs []int, bank tpm2.Algorithm, policyH
 	}
 	defer tpm2.FlushContext(dev, objectHandle)
 
+	// before passing in the PIN, compute the hash then stringify
+	// TODO: research an appropriate buffer size
 	var buffer tpmutil.U16Bytes
 	in := bytes.NewBuffer(password)
 	if err := buffer.TPMUnmarshal(in); err != nil {
 		return nil, fmt.Errorf("tpmutil: failed when unmarshalling buffer")
 	}
+	// assuming the digest is already truncated
 	digest, _, err := tpm2.Hash(dev, bank, buffer, objectHandle)
 	if err != nil {
 		return nil, fmt.Errorf("tpm2: failed when computing hash for buffer")
